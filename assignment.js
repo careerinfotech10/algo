cf497ca10ceeed31baf512a48c11a8a84f190ee4
akshaygagare91 <akshaygagare8@gmail.com>
2022-11-04 14:58:23 +0530

    addedfile

---
+++ assignment.js
@@ -0,0 +1,72 @@
+// *assignmnt -01*
+
+//  let a=prompt("enter 1st num");
+//      a=parseInt(a);
+//  let b=prompt("enter 2nd num");
+//     b=parseInt(b);
+//   let sum=a+b
+//  document.write(sum)
+
+
+//   *assignment -02*
+
+// let a=prompt("add the 1st num");
+//     a=parseInt(a);
+// let b=prompt("enter the 2nd num");
+//     b=parseInt(b);
+// let c=prompt("enter the 3rd num");
+//     c=parseInt(c)
+
+// if(a>b) {
+//     document.write("a is the largest number"+ a)
+// } 
+
+// else if(b>c){
+//     document.write("b is the largest number"+ b)
+// }
+
+// else if(a>c){
+//     document.write("c is the largest number "+ c)
+// }
+
+
+    // *assignment-03*
+
+
+// let income=prompt("enter the income value ");
+//     income=parseInt(income);
+// let cost=prompt("enter the cost value");
+//     cost=parseInt(cost);
+// let sum=income-cost
+//     if(income>=cost){
+//      document.write("profit is :" +sum)
+//     }
+//     else{
+//         document.write("loss is :"+sum)
+//     }
+
+
+//    *assignment-04*
+
+
+// let num1=100;
+// let num2=200;
+// let average=(num1+num2)/2;
+// document.write(average);
+
+
+    
+    //   *assignment -05*
+
+
+    // let i=prompt("enter the i value")
+    //     i=parseInt(i);
+    // let b=prompt("enter the b value ")
+    //     b=parseInt(b);
+    //     area=i*b;
+    // document.write(area)
+
+      
+
+
+
