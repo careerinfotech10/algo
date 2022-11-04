ffb1f054b19627ef331156715ada387c62ea72b9
faisal5942 <faisalmapare747@gmail.com>
2022-11-04 15:00:30 +0530

    File-Added

---
+++ assignment.js
@@ -0,0 +1,75 @@
+
+                                            // .........Assignment-1..........
+
+
+// let n1=prompt("Enter First Number")
+// n1=parseInt(n1)
+// let n2=prompt("Enter Second Number")
+// n2=parseInt(n2)
+// let sum=n1+n2;
+// document.write("Addition of two numbers are:"+sum)
+
+
+                                              //.........Assignment-2...........
+
+
+// let a=prompt("Enter First Number")
+// a=parseInt(a)
+// let b=prompt("Enter Second Number")
+// b=parseInt(b)
+// let c=prompt("Enter Third Number")
+// c=parseInt(c)
+// if(a>b){
+//     document.write("a is greatest"+a)
+// }
+// else if(a>b){
+//     document.write("b is greatest"+b)
+// }
+// else if(c>a){
+//     document.write("c is greatest"+c)
+// }
+
+
+                                            //.........Assignment-3...........
+
+// let income=prompt("Enter your income");
+// // income=parseInt(income)
+// let cost=prompt("Enter your cost");
+// // prompt=parseInt(prompt)
+// if(income>=cost){
+//     document.write(income-cost +":Profit");
+// }
+// else if(cost>=income){
+//     document.write(cost-income +":Loss");
+// }
+
+
+                                            //.........Assignment-4...........
+
+// let num1=prompt("Enter first number");
+// num1=parseInt(num1)
+// let num2=prompt("Enter Second number");
+// num2=parseInt(num2)
+// let average=(num1+num2)/2
+// document.write(average)
+
+                                            //.........Assignment-5...........
+
+// let l=10;
+// let b=20;
+// let area=l*b;
+// document.write("Area of a rectangle:"+area)
+
+                                            //.........Assignment-6...........
+
+let p=10;
+let r=50;
+let t=60;
+let SI=p*r*t;
+document.write("Simple-Interest:"+SI)
+
+
+
+
+
+
