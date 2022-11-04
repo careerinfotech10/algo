d73c0edfe727d0a1f806cd82eda632235519cc71
imrahulverma <rahul271096@gmail.com>
2022-11-04 20:12:10 +0530

    flowchart

---
+++ rahulKumar/que1.js
@@ -0,0 +1,5 @@
+let n1,n2;
+n1=10;
+n2=15;
+ let sum=n1+n2;
+ console.log(sum);
---
+++ rahulKumar/que4.js
@@ -0,0 +1,7 @@
+let num1,num2,average;
+num1=prompt("Enter 1st number")
+num2=prompt("Enter 2nd number")
+num1=Number(num1)
+num2=Number(num2)
+average=(num1+num2)/2
+alert("Average:"+average)
