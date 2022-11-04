9493ebc50a28ec6713e68395439a5a1201b6058e
atul tilekar <atultilekar20@gmail.com>
2022-11-04 14:28:39 +0530

    New file added

---
+++ assignment1.js
@@ -0,0 +1,78 @@
+ // Q1.
+
+// let n1=prompt("enter first number")
+// n1=parseInt(n1)
+// let n2=prompt("enter second number")
+// n2=parseInt(n2)
+// sum=n1+n2 
+// document.write("Addition of two number is :"+sum)
+
+
+// Q2.
+// let a=prompt("enter first number")
+// a=parseInt(a)
+// let b=prompt("enter second number")
+// b=parseInt(b)
+// let c=prompt("enter third number")
+// c=parseInt(c)
+
+// if(a>b){
+//     document.write("Largest Number Is :"+a)
+// }
+// else if(b>c){
+//     document.write("Largest Number Is :"+b)
+// }
+// else if(c>a){
+//     document.write("Largest Number Is :"+c)
+// }
+
+
+// Q3.
+
+// let Income=prompt("Enter income")
+// Income=parseInt(Income)
+// let Cost=prompt("Enter Cost")
+// Cost=parseInt(Cost)
+// let sum=Income-Cost
+
+// if(Income>=Cost){
+//     document.write("Profit Is :"+sum)
+// }
+// else{
+//     document.write("Loss Is :"+sum)
+// }
+
+
+// Q4.
+
+// let num1=prompt("enetr first value")
+// num1=parseInt(num1)
+// let num2=prompt("enter second number")
+// num2=parseInt(num2)
+// let average=num1+num2%2
+
+// document.write("Average Is :"+average)
+
+
+// Q5.
+
+// let i=prompt("enter number")
+// i=parseInt(i)
+// let b=prompt("enter number")
+// b=parseInt(b)
+// let area=i*b
+
+// document.write("Area :"+area)
+
+
+//Q6.
+
+let p=prompt("enter p value")
+p=parseInt(p)
+let r=prompt("enter r value")
+r=parseInt(r)
+let t=prompt("enetr t value")
+t=parseInt(t)
+let SI=p*r*t/100
+
+document.write("SI :"+SI) 
