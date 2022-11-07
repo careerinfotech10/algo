51b4eeb93a1dca6ab7a87643fd347a8ee303e161
RonLog <roshan7715singh@gmail.com>
2022-11-07 11:30:57 +0530

    assignment4 added object, array of object,days,fruits

---
+++ Roshan_assignment/days.js
@@ -0,0 +1,6 @@
+let input=Number(prompt("Input a number value for Days from 1 to 7"));
+// input=Number(input)
+
+let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+alert(days[input-1]);
---
+++ Roshan_assignment/object.js
@@ -0,0 +1,10 @@
+
+let student= {
+ id:101,
+ firstName:'roshan',
+ lastName:'singh',
+ email:'roshan@gmail.com',
+ batch:'gamma'
+
+ }
+console.log(" Student ID : "+student.id+" <br> "+" Student Name :"+student.firstName+"<br>"+" Last Name :"+student.lastName+"<br> "+" Student Batch: "+student.batch); 
