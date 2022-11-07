50ed5d3035afc257ef105648ab93bfc376d79325
Bawangade <akshaybawangade0707@gmail.com>
2022-11-07 17:12:14 +0530

    added assignment

---
+++ AB_Folder/doWhile.js
@@ -0,0 +1,6 @@
+let akshay="yes"
+do{
+    akshay=prompt('Do you want to continue yes/no')
+    console.log('Enter your order');
+
+}while(akshay==="yes");
---
+++ AB_Folder/while.js
@@ -0,0 +1,70 @@
+// // while
+// let num=10;
+// while(num<=20)
+// {
+// console.log(num);
+// num++;
+// }
+
+// // do while
+
+//  let num1=20;
+//  do
+//  {
+//     num1++;
+//     console.log(num1);
+    
+//  }while(num1<30);
+//  // reverser loop
+  
+//  for(let a=10;a>=1;a--)
+//  {
+//    console.log(a);
+//  }
+//  // reverse while
+// let b=100
+//  while( b>=90)
+//  {
+//  b--;
+//  console.log(b);
+// }
+ //function
+ // function is a named block or unnamed block or anunomous block
+ // it represent functionality 
+ // it perform specific task
+
+ function fun()
+ {
+   console.log("hello i am akshay")
+ }
+ fun();
+ //2nd function practice
+ function bawangade()
+ {
+   console.log("2nd function bawangade");
+
+ }
+ bawangade();
+
+ //
+ let num2;
+ let num3;
+ let total;
+ 
+function input()
+ {
+   num2=prompt("num2");
+ num3=prompt("num3");
+}
+ function add()
+ {
+ total=num2+num3;
+ console.log("add---"+total);
+ }
+ function sub()
+ {
+ total=num2-num3;
+ console.log("minsu---"+total);
+ }
+ input();
+sub();
