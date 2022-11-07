bcf98a6a9da6e38ab183528d00e4ed88ff9fdb24
RonLog <roshan7715singh@gmail.com>
2022-11-07 11:52:00 +0530

    assignment added

--- Roshan_assignment/days.js
+++ Roshan_assignment/days.js
@@ -1,5 +1,5 @@
 let input=Number(prompt("Input a number value for Days from 1 to 7"));
-// input=Number(input)
+
 
 let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
