5b4bbc46f303fdaa601355b0d654edd161685c5a
akshaygagare91 <akshaygagare8@gmail.com>
2022-11-07 13:25:30 +0530

    added new file

---
+++ array&object.js
@@ -0,0 +1,39 @@
+// let fruits=["apple","mango","banana"];
+// for(index=0;index<fruits.length;index++){
+//     document.write(fruits[index]+ "<br>")
+// }
+ 
+let days=["sun","mon","tue","wed","thu","fri","sat"]
+let num=prompt("enter value");
+    num=parseInt(num);
+switch(num){
+    case 0:
+    document.write(days[0]);
+    break;
+
+    case 1:
+    document.write(days[1])
+    break;
+
+    case 2:
+    document.write(days[2])
+    break
+
+    case 3:
+    document.write(days[3])
+    break
+
+    case 4:
+    document.write(days[4])
+    break
+
+    case 5:
+    document.write(days[5])
+    break
+
+    case 6:
+    document.write(days[6])
+    break
+}
+
+
