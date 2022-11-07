dc6cda9ae58bd14c8030c441b0241d73c830a34c
imrahulverma <rahul271096@gmail.com>
2022-11-07 09:37:09 +0530

    object and array assignment

---
+++ rahulKumar/Object&Array01.js
@@ -0,0 +1,13 @@
+let emp={
+    id:1001,
+    firstName:"Rahul",
+    lastName:"Kumar",
+    email:"rahul271096@gmail.com",
+    salary:20000
+}
+
+console.log("Employee Id: "+emp.id)
+console.log("First Name: "+emp.firstName)
+console.log("Last Name: "+emp.lastName)
+console.log("Email ID: "+emp.email)
+console.log("Salary: "+emp.salary)
