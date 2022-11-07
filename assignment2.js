d0d3b307f76fb08dd4316dc9a8578ab08dcbef57
atul tilekar <atultilekar20@gmail.com>
2022-11-07 12:29:51 +0530

    assignment2

---
+++ assignment2.js
@@ -0,0 +1,101 @@
+// let student={
+//     firstName:"Atul",
+//     LastName:"Tilekar",
+//     RollNo:15,
+//     MobileNo:9987410847
+// }
+
+// document.write("Student Name :"+student.firstName+"<br>"+"Last Name :"+student.LastName+"<br>"
+//              +"Roll No :"+student.RollNo+"<br>"+"Mobile No :"+student.MobileNo)
+
+
+// Q2
+
+// let employees=[
+//     {
+//    firstName:'Atul',
+//    lastName:'Tilekar',
+//    email:'atultilekar15@gmail.com',
+//    salary:10000
+//     },
+//     {
+//     firstName:'Ashutosh',
+//     lastName:'Pawar',
+//     email:'ashutoshpawar5@gmail.com',
+//     salary:150000
+//     }
+//    ]
+
+//    for(let i=0;i<=2;i++)
+//    document.write(employees[i].firstName +" "+employees[i].lastName+" "+employees[i].email+" "+
+//                 employees[i].salary+"<br>")
+
+
+//Q3
+
+// let fruit=["papaya"+" ,"+ "pineapple"+", "+"watermelon"]
+
+// document.write(fruit)
+   
+
+// Q4
+
+let days=["Enter From 1 - 7","Sun","Mon","Tue","Wen","Thu","Fri","Sat"]
+let i;
+let input=prompt("enter number")
+input=parseInt(input) 
+document.write(days[input])
+// for( i=0;i<days.length;i++){
+//    if(input==days.lenght){
+//     break;
+//    }
+// }
+// document.write(days[input])
+
+// Q4
+
+// let days=prompt("Enter Number")
+// days=parseInt(days)
+
+// switch(days){
+
+// case 1:
+//     document.write("Sunday")
+//     break
+
+// case 2:
+//     document.write("Monday")    
+//     break
+
+// case 3:
+//     document.write("Tuesday")    
+//     break
+
+// case 4:
+//     document.write("Wensday")
+//     break
+    
+// case 5:
+//     document.write("Thursday")    
+//     break
+
+// case 6:
+//     document.write("Friday")    
+//     break
+
+// case 7:
+//     document.write("Saturday")
+//     break
+    
+// default:
+//     document.write("Enter Vaild Number")    
+
+// }
+
+
+
+
+
+
+
+
