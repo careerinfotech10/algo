49c0329c1ac1db820297bfa9da7e47207cafbdb1
utkarsh18032 <utkarshpatil1834@gmail.com>
2022-11-08 10:46:41 +0530

    array duplicate number find out added

---
+++ UTKARSH/arrayque2.js
@@ -0,0 +1,16 @@
+array=[10 , 8 , 77 , 66, 55, 8 , 8]
+// Que.1 ) find duplicate number .
+// Que.2 ) find t he duplicate number and its occurance.
+
+// we have to use loop here
+
+for (let i=0 ; i<7 ; i++){
+
+for(let j=i+1; j<7 ; j++){
+
+    if(array[i]===array[j])
+    console.log(array[i]===array[j])
+}
+
+return -1
+}
