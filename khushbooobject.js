cac5cd01a018227dcd5dd472f307e8a245242fd9
khushbookhan <khushbookhan15901@gmail.com>
2022-11-08 12:20:34 +0530

    file added by khushboo

---
+++ khushbooobject.js
@@ -0,0 +1,60 @@
+            // ----------------------- Q.1--------------------
+
+// let phone={
+//     brand:"apple",
+//     model: "iphone",
+//     storage:64,
+//     price:50000,
+//     ram:8,
+//     color:"red",
+
+
+
+// }
+
+// document.write("brand :"+phone.brand +"<br>" +"model :" +phone.model+"<br>" +"storage :"+phone.storage +"<br>" +"price :"+phone.price)
+
+
+                //   -----------------Q.2----------------
+
+//   let employees=[
+//     {
+//         firstname:"khushboo",
+//         lastname:"khan",
+//         email:"gmail.com",
+//         salary:20000,
+        
+//     },
+
+//     {
+//         firstname:"faisal",
+//         lastname:"mapare",
+//         email:"gmail.com",
+//         salary:25000,
+
+//     }
+
+//   ]
+
+//   for(let i=0;i<2;i++){
+//     console.log(employees[i].firstname," ",employees[i].lastname," ",employees[i].email,employees[i].salary," ")
+//   }
+    
+
+                //    -----------------Q.3-----------------
+
+
+    // let fruits=["apple","grapes","mango","cherry"] 
+    // console.log(fruits)           
+  
+
+                    //    ---------------------Q.4-----------------
+
+
+    //  let days=["sun","mon","tue","wed","thurs","fri","sat"]
+    //  let input=prompt("enter the number")
+    //  document.write(days[input])
+
+     
+    
+  
